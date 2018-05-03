d8359cca9c3a7787791be0499c10e65e7c1f5654
Saxon Evers <hello@saxons.place>
2018-05-03 12:35:34 +1000

    header size

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,7 +33,7 @@ class Header extends React.Component {
               <div className='container'>
                 <div className='col-lg-6 col-md-6 col-sm-6 col-xs-6'>
                   <div className='row'>
-                    <div className='col-lg-4 col-md-4 col-sm-12 col-xs-12'>
+                    <div className='col-lg-4 col-md-4 col-sm-12 col-xs-12 header_image'>
                       <NavLink
                         onClick={this.closeModal}
                         className='navbar-brand'
