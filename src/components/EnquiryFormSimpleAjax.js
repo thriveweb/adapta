ccac0405b18fcc2ab7520e057ebf9683418f4052
Dean Oakley <contact@thriveweb.com.au>
2018-09-05 12:06:11 +1000

    Add captcha

--- src/components/EnquiryFormSimpleAjax.js
+++ src/components/EnquiryFormSimpleAjax.js
@@ -131,7 +131,8 @@ class Form extends React.Component {
                 justifyContent: 'center'
               }}
             >
-             
+              
+             <div data-netlify-recaptcha></div>
 
               <input
                 className='view-products'
